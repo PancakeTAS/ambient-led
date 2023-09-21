f290a1b15befd7733296733670445714e1de39fc
PancakeTAS <pancake@mgnet.work>
2023-09-21 23:57:15 +0200

    Update arduino led

--- src/main/java/de/pancake/ambientled/host/arduino/ArduinoLed.java
+++ src/main/java/de/pancake/ambientled/host/arduino/ArduinoLed.java
@@ -1,15 +1,16 @@
-package de.pancake.led.host;
+package de.pancake.ambientled.host.arduino;
 
 import com.fazecast.jSerialComm.SerialPort;
 
+import java.awt.*;
 import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * Led strip
+ * Led controller for Arduino based led strips
  * @author Pancake
  */
-public class Led {
+public class ArduinoLed {
 
     private final String name;
     private final SerialPort device;
@@ -20,27 +21,31 @@ public class Led {
      * @param name Name of the com port
      * @throws Exception If the com port couldn't be opened
      */
-    public Led(String name) throws Exception {
+    public ArduinoLed(String name) throws Exception {
         this.name = name;
         this.device = this.findComPort();
         this.device.openPort();
         this.device.setBaudRate(38400);
         this.stream = this.device.getOutputStream();
+    }
 
+    /**
+     * Clear the led strip
+     * @throws IOException If the data couldn't be written
+     */
+    public void clear() throws IOException {
         for (int i = 0; i < 180; i++)
-            this.write(i, 0, 0, 0);
+            this.write(i, Color.BLACK);
     }
 
     /**
      * Write color data to the led strip
      * @param i Index
-     * @param r Red
-     * @param g Green
-     * @param b Blue
+     * @param c Color
      * @throws IOException If the data couldn't be written
      */
-    public void write(int i, int r, int g, int b) throws IOException {
-        this.stream.write(new byte[] { (byte) (i << 8), (byte) i, (byte) r, (byte) g, (byte) b });
+    public void write(int i, Color c) throws IOException {
+        this.stream.write(new byte[] { (byte) (i << 8), (byte) i, (byte) c.getRed(), (byte) c.getGreen(), (byte) c.getBlue() });
     }
 
     /**
@@ -66,9 +71,13 @@ public class Led {
     /**
      * Close the connection to the Arduino
      * @throws IOException If the connection couldn't be closed
+     * @return null
      */
-    public void close() throws IOException {
+    public ArduinoLed close() throws IOException {
+        this.clear();
+        this.flush();
         this.stream.close();
         this.device.closePort();
+        return null;
     }
 }
